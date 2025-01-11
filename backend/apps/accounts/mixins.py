ac85a849256ff0067877e791993c893279fef914
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2025-01-12 00:31:28 +0700

    feat: add CreateModelMixin and UpdateModelMixin for handling create and update operations

---
+++ backend/apps/accounts/mixins.py
@@ -0,0 +1,42 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.viewsets import GenericViewSet
+
+
+class CreateModelMixin(GenericViewSet):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(status=status.HTTP_201_CREATED, headers=headers)
+
+    @staticmethod
+    def perform_create(serializer):
+        serializer.save()
+
+    @staticmethod
+    def get_success_headers(data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
+
+
+class UpdateModelMixin(GenericViewSet):
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response()
+
+    @staticmethod
+    def perform_update(serializer):
+        serializer.save()
