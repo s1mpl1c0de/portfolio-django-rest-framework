514509ea3bb7d6f9e10626253ef0e063f0205770
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2025-01-12 01:04:35 +0700

    feat: create ModelViewSet combining common mixins

---
+++ backend/common/viewsets.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+
+from common.mixins import (
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+    ReadWritableSerializerMixin,
+)
+
+
+class ModelViewSet(
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+    ReadWritableSerializerMixin,
+    viewsets.ModelViewSet,
+):
+    lookup_field = 'uuid'
