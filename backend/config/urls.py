1e9308c9b58bea45f5784ce2a16676b646a48e33
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2025-01-12 00:40:38 +0700

    feat: add UserViewSet to API router and configure API versioning

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,10 +9,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from apps.accounts.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, 'user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include(router.urls)),
 ]
 
 if settings.DEBUG:
